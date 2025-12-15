21ec2885c1f49ca0ce6132f8d6a93f25d521a348
jwy-hm <gmzhaoyubo@gmail.com>
2025-12-16 02:23:09 +0800

    第二次更改

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  title: "潮流周刊",
+  title: "我的周刊",
   author: "jwy_hm",
   description: "记录工程师 jwy_hm 的不枯燥生活，每周一发布，欢迎关注",
   keywords: "jwy_hm,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计",
