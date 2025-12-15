df11735cbd57e56c2cc8e4301df4402ec28b13f8
jwy-hm <gmzhaoyubo@gmail.com>
2025-12-16 02:49:54 +0800

    更改page位置

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,10 @@ export const SITE = {
   icon: "https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png",
   siteImage:
     "https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png",
-  homePage: "https://weekly.tw93.fun",
-  blogPage: "https://tw93.fun/",
+  //homePage: "https://weekly.tw93.fun",
+  //blogPage: "https://tw93.fun/",
+  homePage: "http://safehome.eu.org/",
+  blogPage: "http://safehome.eu.org/",
   twitterId: "HiTw93",
   githubId: "jwy-hm",
   repo: "jwy-hm/Weekly",
